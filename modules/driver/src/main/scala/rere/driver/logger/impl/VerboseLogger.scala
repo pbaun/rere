aebba0a188f497028232e8e19a17e6d7f3f3fd59
Pavel Baun <baunpavel@gmail.com>
2017-05-16 03:09:07 +0300

    !drv Remove custom logger

--- modules/driver/src/main/scala/rere/driver/logger/impl/VerboseLogger.scala
+++
@@ -1,9 +0,0 @@
-package rere.driver.logger.impl
-
-import rere.driver.logger.{Logger, LoggingGroup}
-
-class VerboseLogger extends Logger {
-  override def log(group: LoggingGroup, id: String, message: String): Unit = {
-    println(s"[$group][$id] $message")
-  }
-}
